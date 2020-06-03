d4648803327884d3241fa3fa7e7be8f3ec5bb649
Relton Fidelis ME <contato.reltonfidelis@gmail.com>
2020-06-03 00:00:04 -0300

    add the system info class

---
+++ src/main/java/br/com/saudefacil/system/SystemData.java
@@ -0,0 +1,14 @@
+package br.com.saudefacil.system;
+
+public class SystemData {
+	
+	private String sitemVersion;
+
+	public String getSitemVersion() {
+		return sitemVersion;
+	}
+
+	SystemData(){
+		this.sitemVersion = "1.0";
+	}
+}
