f6bf0d0b0f71d8419a8578bcf0503d2f3111143e
Rennan Cândido <66339731+Rennan-sbarros@users.noreply.github.com>
2020-06-07 19:04:52 -0300

    Update 8_TabelaSintomas.sql

--- src/SQLDatabase/BD_SAUDE_FINAL/8_TabelaSintomas.sql
+++ src/SQLDatabase/BD_SAUDE_FINAL/8_TabelaSintomas.sql
@@ -1,12 +1,14 @@
-
-	insert into sintomas values 
-	(1001, 'Dor de cabeça'),
-	(1002, 'Dor de dente'),
-	(1003, 'Febre'),
-	(1004, 'Irritação nos olhos'),
-	(1005, 'Tosse'),
-	(1006, 'Coriza'),
-	(1007, 'Falta de ar'),
-	(1008, 'Formigamento nos membros'),
-	(1009, 'Calafrio'),
-	(1010, 'dor de garganta');
+insert into sintomas (descricao) 
+    values 
+	('Dor de cabeÃ§a'),
+	('Dor de dente'),
+	('Febre'),
+	('IrritaÃ§Ã£o nos olhos'),
+	('Tosse'),
+	('Coriza'),
+	('Falta de ar'),
+	('Formigamento nos membros'),
+	('Calafrio'),
+	('dor de garganta');
+    
+    select * from sintomas
